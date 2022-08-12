2044d6c3581e6bb222d1babf7fb7e1c8a2d33505
lionelguglielmone <lionel@guglielmone.com>
2022-08-12 17:36:44 -0300

    lesson 2-3

---
+++ DH-lessons2022_C2/src/week_2/lesson3/Impresora.java
@@ -0,0 +1,59 @@
+package week_2.lesson3;
+
+import java.util.Calendar;
+
+public class Impresora {
+    //atributos
+    private String modelo;
+    private Boolean conexionInalambrica;
+    private Calendar fechaFabricacion;
+    private Integer cantHojas;
+    private Boolean prendida;
+
+
+    //constructor
+    public Impresora(){
+        cantHojas = 0;
+        this.prendida = false;
+    }
+
+    public Impresora(Integer cantidadHojas){
+        cantHojas = cantidadHojas;
+        this.prendida = false;
+    }
+
+    //metodos
+    public void prender(){
+        this.prendida = true;
+    }
+    public void apagar(){
+        this.prendida = false;
+    }
+
+    public void cargarHojas(Integer cantHojas){
+        this.cantHojas += cantHojas;
+    }
+
+    public Boolean isPrendida(){
+        return this.prendida;
+    }
+
+    public Integer getCantHojas(){
+        return this.cantHojas;
+    }
+
+    public Boolean imprimir(String texto, Integer cantHojas){
+        if(!this.prendida){
+            System.out.println("Impresora apagada");
+            return false;
+        }
+        if(this.cantHojas<cantHojas){
+            System.out.println("no hay hojas");
+            return false;
+        }
+        System.out.println("Su impresion: " + texto);
+        this.cantHojas -= cantHojas;
+        return true;
+    }
+
+}
---
+++ DH-lessons2022_C2/src/week_2/lesson3/Test.java
@@ -0,0 +1,24 @@
+package week_2.lesson3;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+    Impresora impresora = new Impresora();
+    /*
+    System.out.println("esta prendida: " + impresora.isPrendida());
+    impresora.prender();
+    System.out.println("esta prendida: " + impresora.isPrendida());
+    System.out.println("cantidad de hojas: " + impresora.getCantHojas());
+
+    impresora.cargarHojas(10);
+    System.out.println("cantidad de hojas: " + impresora.getCantHojas());
+    impresora.cargarHojas(10);
+    System.out.println("cantidad de hojas: " + impresora.getCantHojas());
+    */
+    impresora.prender();
+    impresora.cargarHojas(10);
+    impresora.imprimir("Bienvenidos a la segunda semana de POO", 10);
+    System.out.println("cantidad de hojas: " + impresora.getCantHojas());
+    }
+}
